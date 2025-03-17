3584e60d01e37a8f74f72438b6f6ad4167cb815d
SeifEldin Hany <seif20062007@gmail.com>
2025-03-17 19:53:27 +0100

    Error handling of the user input
    Now different values are not accepted in the code

--- Functions.py
+++ Functions.py
@@ -23,23 +23,31 @@ def MainMenu() -> str:
     global UserStart
     print("1. Start Quiz")
     print("2. Go through results")
-    try:
-        UserStart = int(input("Please Choose (1 or 2) (to exit enter 0) "))
-    except ValueError:
-        print("Invalid input, Correct values ar (1 or 2)")
-    if UserStart == 0:
-        sys.exit()
+    while True:
+        try:
+            UserStart = int(input("Please Choose (1 or 2) (to exit enter 0) "))   
+        except ValueError:
+            print("Invalid input, Correct values ar (1 or 2)")
+            continue
+        break
+    #if UserStart == 0:
+    #   sys.exit()
+                
 
 def Topics():
     global Topic, Start, Elapsed, Limit
+    
     print("===============")
     print("1. Math")
     print("2. Physics")
     print("3. English")
-    try:    
-        Topic = int(input("Please choose a topic from the list above (1, 2, 3) "))
-    except ValueError:
-        print("Invalid input please enter (1, 2 or 3)")
+    while True:    
+        try:    
+            Topic = int(input("Please choose a topic from the list above (1, 2, 3) "))
+        except ValueError:
+            print("Invalid input please enter (1, 2 or 3)")
+            continue
+        break   
     Elapsed = 0
     Limit = 10
     Start = time.time()
@@ -48,7 +56,7 @@ def Topics():
 
 def Display() -> str:
     for x in Question: 
-        print(str(*x[:-1]))
+        print(str(*x[:-1])) #this converts the item in the array to str so that it can remove Curly brackets that appear when outputing the question
     for Answers in Options:
         print(Answers)
 #this goes through the Subject dictionary to randomize and pick a question from a specific subject 
@@ -57,42 +65,39 @@ def randomizer() -> dict:
     Question = []
     Options = []
     Ans = []
-    try:
-        if Topic == 1:
-            Rq = random.choice(list(Subject["Math"].items()))
-            Quiz = copy.deepcopy(Rq)
-            Ans.append(Rq[1]["Answer"])
-            Options.append(Rq[1]["A"])
-            Options.append(Rq[1]["B"])
-            Options.append(Rq[1]["C"])
-            Options.append(Rq[1]["D"])
-            Quiz[1].clear()
-            Question.append(Quiz)
-            Display()
-        elif Topic == 2:
-            Rq = random.choice(list(Subject["Physics"].items()))
-            Quiz = copy.deepcopy(Rq)
-            Ans.append(Rq[1]["Answer"])
-            Options.append(Rq[1]["A"])
-            Options.append(Rq[1]["B"])
-            Options.append(Rq[1]["C"])
-            Options.append(Rq[1]["D"])
-            Quiz[1].clear()
-            Question.append(Quiz)
-            Display()
-        elif Topic == 3:
-            Rq = random.choice(list(Subject["English"].items()))
-            Quiz = copy.deepcopy(Rq)
-            Ans.append(Rq[1]["Answer"])
-            Options.append(Rq[1]["A"])
-            Options.append(Rq[1]["B"])
-            Options.append(Rq[1]["C"])
-            Options.append(Rq[1]["D"])
-            Quiz[1].clear()
-            Question.append(list(Quiz))
-            Display()
-    except:
-            print("Invalid input")
+    if Topic == 1:
+        Rq = random.choice(list(Subject["Math"].items()))
+        Quiz = copy.deepcopy(Rq)
+        Ans.append(Rq[1]["Answer"])
+        Options.append(Rq[1]["A"])
+        Options.append(Rq[1]["B"])
+        Options.append(Rq[1]["C"])
+        Options.append(Rq[1]["D"])
+        Quiz[1].clear()
+        Question.append(Quiz)
+        Display()
+    elif Topic == 2:
+        Rq = random.choice(list(Subject["Physics"].items()))
+        Quiz = copy.deepcopy(Rq)
+        Ans.append(Rq[1]["Answer"])
+        Options.append(Rq[1]["A"])
+        Options.append(Rq[1]["B"])
+        Options.append(Rq[1]["C"])
+        Options.append(Rq[1]["D"])
+        Quiz[1].clear()
+        Question.append(Quiz)
+        Display()
+    elif Topic == 3:
+        Rq = random.choice(list(Subject["English"].items()))
+        Quiz = copy.deepcopy(Rq)
+        Ans.append(Rq[1]["Answer"])
+        Options.append(Rq[1]["A"])
+        Options.append(Rq[1]["B"])
+        Options.append(Rq[1]["C"])
+        Options.append(Rq[1]["D"])
+        Quiz[1].clear()
+        Question.append(list(Quiz))
+        Display()
 
         
 
@@ -104,8 +109,11 @@ def Guess():
     """
     Timer = Limit - Elapsed.__round__(0)
     print(f"You have {Timer}s left")
-    try:
+    try: 
         Guess = inputimeout(prompt = "please enter a guess to the question (A, B, C, D) ", timeout = Timer)
+        while Guess != "A" and Guess != "B" and Guess != "C" and Guess != "D":
+            print("incorrect input, must be (A, B, C or D)")
+            Guess = inputimeout(prompt = "please enter a guess to the question (A, B, C, D) ", timeout = Timer)
         for key in Rq:
             if Ans[0] == Guess:
                 print("Correct")
@@ -125,10 +133,9 @@ def Viewing():
         print(x)
     View.close()        
 def Scores():
-    Viewing()
     Score = open("Scores.txt", "a")
-    if View == Username:
-        Score.write(f"{Username}, You got {Count} Questions correct. \n")
+
+    Score.write(f"{Username}, You got {Count} Questions correct. \n")
     
     Score.close()
 
--- main.py
+++ main.py
@@ -2,33 +2,29 @@ import time
 import Functions
 
 def main():
-    
     Functions.Intro()
     while True: #indefinite loop until user wants to exit 
         Functions.MainMenu()
-        try:
-            if Functions.UserStart == 1:
-                Functions.Topics()
-                if Functions.Elapsed < Functions.Limit:
-                    for i in range(3):
-                        if Functions.Elapsed >= 10 :
-                            print("Times up returning to main menu")
-                            time.sleep(1)
-                        else:
-                            print("Question",i+1)
-                            Functions.randomizer()
-                            Functions.Guess()
-                            Functions.Question.pop()
-                            Functions.Options.pop()
-                            Functions.Elapsed = time.time() - Functions.Start
-                            print(Functions.Elapsed.__round__(0))
-                            
-                        print("===============")
-                    Functions.Scores()
-            elif Functions.UserStart == 2:
-                Functions.Viewing()
-        except ValueError:
-            print("Invalid input please enter the correct values are ")
+        if Functions.UserStart == 1:
+            Functions.Topics()
+            if Functions.Elapsed < Functions.Limit:
+                for i in range(3):
+                    if Functions.Elapsed >= 10 :
+                        print("Times up returning to main menu")
+                        time.sleep(1)
+                    else:
+                        print("Question",i+1)
+                        Functions.randomizer() #this randomizes the questions
+                        Functions.Guess()
+                        Functions.Question.pop()
+                        Functions.Options.pop()
+                        Functions.Elapsed = time.time() - Functions.Start
+                        print(Functions.Elapsed.__round__(0))
+                        
+                    print("===============")
+                Functions.Scores()
+        elif Functions.UserStart == 2:
+            Functions.Viewing()
 
 
 
