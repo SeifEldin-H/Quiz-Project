295422db0f1962446aafccf8fdc166d8bcf7c17c
SeifEldin Hany <142739946+SeifEldin-H@users.noreply.github.com>
2025-03-17 16:56:14 +0100

    Quiz files
    All neccessary files for my quiz to run

---
+++ Questions.py
@@ -0,0 +1,198 @@
+Subject ={
+    "Math":{
+        "Substitute x in 3x+2 where x = 2":{
+            "A":"A: 4",
+            "B":"B: 8",
+            "C":"C: 2",
+            "D":"D: 10",
+            "Answer":"B"
+        },
+        "Solve for x 5x+3 = 7":{
+            "A":"A: 1/4",
+            "B":"B: 3/4",
+            "C":"C: 1/2",
+            "D":"D: 4/5",
+            "Answer":"D"
+        },
+        "What is ":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"B"
+        },
+        "Question 4":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"A"
+        },
+        "Question 5":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"A"
+        },
+        "Question 6":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"D"
+        },
+        "Question 7":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"B"
+        },
+        "Question 8":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"A"
+        },
+        "Question 9":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"C"
+        },
+        "Question 10":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"C"
+        }
+    },
+    "Physics":{
+        "For the hydrogen atom, which series describes electron transitions to the N=1 orbit, the lowest energy electron orbit?":{
+            "A":"A: Paschen series",
+            "B":"B: Lyman series",
+            "C":"C: Balmer series",
+            "D":"D: Pfund series",
+            "Answer":"B"
+        },
+        "Two forces have magnitudes of 11 newtons and 5 newtons. The magnitude of their sum could NOT be equal to which of the following values?":{
+            "A":"A: 7 newtons",
+            "B":"B: 5 newtons",
+            "C":"C: 16 newtons",
+            "D":"D: 9 newtons",
+            "Answer":"B"
+
+        },
+        "A pendulum which is suspended from the ceiling of a railroad car is observed to hang at an angle of 10 degrees to the right of vertical. Which of the following answers could explain this phenomena?":{
+            "A":"A: The railroad car is at rest",
+            "B":"B: The railroad car is accelerating to the right.",
+            "C":"C: The railroad car is moving with constant velocity to the right.",
+            "D":"D: The railroad car is accelerating to the left.",
+            "Answer":"D"
+        },
+        "A block of metal which weighs 60 newtons in air and 40 newtons under water has a density, in kilograms per meter cubed, of":{
+            "A":"A: 1000",
+            "B":"B: 5000",
+            "C":"C: 3000",
+            "D":"D: 7000",
+            "Answer":"C"
+        },
+        "In physics, a radian per second is a unit of":{
+            "A":"A: angular velocity",
+            "B":"B: angular displacement",
+            "C":"C: angular acceleration",
+            "D":"D: angular momentum.",
+            "Answer":"A"
+        },
+        "An object of mass 2kg is thrown vertically downwards with an initial kinetic energy of 100J. What is the distance fallen by the object at the instant when its kinetic energy has doubled?":{
+            "A":"A: 2.5m",
+            "B":"B: 5.0m",
+            "C":"C: 10m",
+            "D":"D: 14m",
+            "Answer":"B"
+        },
+        "Which of the following scientists is responsible for the exclusion  principle which states that two objects may NOT occupy the same space at the same time? Was it:":{
+            "A":"A: Heisenberg",
+            "B":"B: Teller",
+            "C":"C: Pauli",
+            "D":"D: Bohr",
+            "Answer":"C"
+        },
+        "Induced electric currents can be explained using which of the following laws?":{
+            "A":"A: Gauss's Law",
+            "B":"B: Ampere's Law",
+            "C":"C: Ohm's Law",
+            "D":"D: Faraday's Law",
+            "Answer":"D"
+        },
+        "For a parallel-plate capacitor with plate area (A) and plate separation (d), the capacitance is proportional to which of the following?":{
+            "A":"A: A divided by d",
+            "B":"B: A times d",
+            "C":"C: d divided by A",
+            "D":"D: A divided by d squared",
+            "Answer":"A"
+        },
+        "Iron is what type of magnetic material? Is it:":{
+            "A":"A: non-magnetic",
+            "B":"B: ferromagnetic",
+            "C":"C: paramagnetic",
+            "D":"D: diamagnetic",
+            "Answer":"B"
+        }
+    },
+    "English":{
+        "What is missing in this sentence: ":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"A"
+        },
+        "Question 5":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"A"
+        },
+        "Question 5":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"A"
+        },
+        "Question 5":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"A"
+        },
+        "Question 5":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"A"
+        },
+        "Question 5":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"A"
+        },
+        "Question 5":{
+            "A":"A: An1",
+            "B":"B: An2",
+            "C":"C: An3",
+            "D":"D: An4",
+            "Answer":"A"
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Requirements:
+python version 3.9 or higher
+pip3 install inputimeout
+pip3 install time
